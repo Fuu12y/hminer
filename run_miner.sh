56891830251656d878133d2d531ae38a498b298f
Fuu12y <vikri.r@gmail.com>
2025-02-26 11:14:42 +0800

    Initial commit

---
+++ run_miner.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# For more usage info: hellminer --help
+
+# Example SSL URL
+# .\hellminer.exe -c stratum+ssl://na.luckpool.net:3958 -u RJ5Xav3AXLdbUUwKNojd38ToSZW9FmE93m.x3 -p hybrid --cpu 4
+
+.\hellminer.exe -c stratum+tcp://na.luckpool.net:3960 -u RJ5Xav3AXLdbUUwKNojd38ToSZW9FmE93m.pc -p hybrid --cpu 4
